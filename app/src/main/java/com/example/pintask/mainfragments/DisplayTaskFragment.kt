d745fbebdc2af47193081b79383c3ecdd855d1df
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-11-30 13:20:32 +0530

    notification can be send

--- app/src/main/java/com/example/pintask/mainfragments/DisplayTaskFragment.kt
+++ app/src/main/java/com/example/pintask/mainfragments/DisplayTaskFragment.kt
@@ -1,10 +1,20 @@
 package com.example.pintask.mainfragments
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RemoteViews
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -16,9 +26,12 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.example.pintask.R
+import com.example.pintask.TaskDetailActivity
 import com.example.pintask.adapter.TaskListAdapter
+import com.example.pintask.constants.AppConstants
 import com.example.pintask.databinding.FragmentDisplayTaskBinding
 import com.example.pintask.firebase.FirestoreFunctions
+import com.example.pintask.model.TaskModel
 import com.example.pintask.model.TaskViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -35,6 +48,12 @@ class DisplayTaskFragment : Fragment() {
     private lateinit var mGoogleSignInClient: GoogleSignInClient
     private lateinit var taskListAdapter: TaskListAdapter
     private val viewModel: TaskViewModel by activityViewModels()
+    private lateinit var notificationChannel: NotificationChannel
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationBuilder: Notification.Builder
+    private val channel_ID = "i.apps.notifications"
+    private val description = "Test notification"
+    private var task_id = 0
 
     override fun onStart() {
         super.onStart()
@@ -62,11 +81,68 @@ class DisplayTaskFragment : Fragment() {
         binding = FragmentDisplayTaskBinding.inflate(inflater, container, false)
         taskListAdapter = TaskListAdapter()
 
+        notificationManager =
+            requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         viewModel.taskList.observe(viewLifecycleOwner, Observer {
             taskListAdapter.submitList(it)
+
+            for(i in it){
+                //send notification
+                // TODO: change id
+                val currentTask = i.toObject(TaskModel::class.java)
+                if(currentTask!!.pinned==true) {
+                    buildNotification(currentTask.taskTitle ?: AppConstants.DEFAULT_TASK_TITLE,
+                        currentTask.task ?: AppConstants.DEFAULT_TASK_DESC)
+                    notificationManager.notify(task_id, notificationBuilder.build())
+                    task_id++
+                }
+            }
+
+
         })
 
         return binding.root
+    }
+
+    @SuppressLint("RemoteViewLayout")
+    private fun buildNotification(taskTitle: String, task: String) {
+        val intent = Intent(requireActivity(), TaskDetailActivity::class.java)
+
+        //immutable since no changes after clicking notification
+        val pendingIntent =
+            PendingIntent.getActivity(requireActivity(), 0, intent, PendingIntent.FLAG_MUTABLE)
+
+        //api>=26 requires notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel =
+                NotificationChannel(channel_ID, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            notificationBuilder = Notification.Builder(requireActivity(), channel_ID)
+                .setSmallIcon(R.drawable.pushpin_selected)
+                .setContentTitle(taskTitle)
+                .setContentText(task)
+                .setContentIntent(pendingIntent)
+                .setOngoing(true) // to keep notification in notification bar
+                .setAutoCancel(true)
+
+        } else {
+
+            val contentView = RemoteViews(requireContext().packageName,R.layout.activity_task_detail)
+
+            notificationBuilder = Notification.Builder(requireActivity())
+                .setContent(contentView)
+                .setSmallIcon(R.drawable.pushpin_selected)
+                .setContentIntent(pendingIntent)
+                .setOngoing(true) // to keep notification in notification bar
+                .setAutoCancel(true)
+
+        }
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
