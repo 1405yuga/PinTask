bcb18d68d33a8d7613f20053eaeb18883c389e9b
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-12-12 11:04:36 +0530

    added task detail fragment

--- app/src/main/java/com/example/pintask/TaskDetailActivity.kt
+++ app/src/main/java/com/example/pintask/TaskDetailActivity.kt
@@ -1,11 +1,23 @@
 package com.example.pintask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class TaskDetailActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHostContainer) as NavHostFragment
+        navController = navHost.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/pintask/taskdetailfragments/DetailsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.pintask.taskdetailfragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pintask.R
+import com.example.pintask.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
